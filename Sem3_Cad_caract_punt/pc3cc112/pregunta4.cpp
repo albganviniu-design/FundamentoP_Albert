d28961b24496a770da5fba482926218eee14cb4d
albganviniu-design <albert.ganvini.u@uni.pe>
2026-01-30 17:02:29 -0500

    Añadiendo pc3 en la semana3

---
+++ Sem3_Cad_caract_punt/pc3cc112/pregunta4.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+using namespace std;
+
+// Utilice los siguientes prototipos
+
+int guardaDirecciones(char* texto, char** indice)
+{
+    int cont = 0;
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+}
+
+
+int comparar(const char* a, const char* b);
+
+
+void ordenaIndice(char** indice, int n);
+
+
+void imprimir(char* texto, char** indice, int n);
+
+int main()
+{
+    char texto[] = "los punteros no se copian se referencian y se ordenan";
+
+
+
+
+    return 0;
+}
